8f9909b5ca5064dc0f101347c55f51ee3149b4cd
Jakub Zajkowski <jakudjan123@gmail.com>
2025-03-11 22:37:54 +0100

    Implement scheduler

---
+++ coinwatch/src/main/java/org/example/coinwatch/CoinGeckoScheduler.java
@@ -0,0 +1,11 @@
+package org.example.coinwatch;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@EnableScheduling
+@Component
+public class CoinGeckoScheduler {
+
+}
