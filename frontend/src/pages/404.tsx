19832ba86cfab9fb2297ac7f46a0879ba495f3ab
jakubzajkowski <jakudjan123@gmail.com>
2025-05-16 18:10:42 +0200

    Implement translation for not found

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,19 +1,28 @@
 import styled from "styled-components";
 import Error from "../components/error.tsx";
+import { useTranslation } from "react-i18next";
 
 const Container = styled.div`
-    padding: 5rem 0.5rem 0 0.5rem;
-    width: 100%;
-    height: 90vh;
-    display: flex;
-    justify-content: center;
-    background-color: ${({ theme }) => theme.colors.secondary};
-`
+  padding: 5rem 0.5rem 0 0.5rem;
+  width: 100%;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.secondary};
+`;
 
-const NotFound = ()=>{
-    return <Container>
-        <Error code={404} title={"Page Not Found"} description={"There is no such a page found."} />
+const NotFound = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <Error
+        code={404}
+        title={t("notFound.title")}
+        description={t("notFound.description")}
+      />
     </Container>
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
