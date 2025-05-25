1c827c65682fa07eb7f61b15d79832305dad2817
jakubzajkowski <jakudjan123@gmail.com>
2025-05-25 22:41:11 +0200

    Implement gemini and repair bugs with esmodules

--- ai/src/index.ts
+++ ai/src/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import { gemini } from './config/gemini.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (_req, res) => {
+app.get('/',async (_req, res) => {
+  async function main() {
+    const response = await gemini.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: "Explain how AI works in a few words",
+    });
+    console.log(response.text);
+  }
+
+  await main();
   res.send('Hello from Express + TypeScript!');
 });
 
