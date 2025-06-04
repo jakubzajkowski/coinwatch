58331af24786eb95e3c3e3d06e6d74be5b65ce12
jakubzajkowski <jakudjan123@gmail.com>
2025-06-05 00:55:10 +0200

    Implement crypto get chart resolver and refactor service for this

---
+++ coinwatch/src/main/java/org/example/coinwatch/dto/ChartType.java
@@ -0,0 +1,7 @@
+package org.example.coinwatch.dto;
+
+public enum ChartType {
+    CANDLE,
+    LINE,
+    BAR
+}
--- coinwatch/src/main/java/org/example/coinwatch/service/CryptoChartService.java
+++ coinwatch/src/main/java/org/example/coinwatch/service/CryptoChartService.java
@@ -1,19 +1,44 @@
 package org.example.coinwatch.service;
 
+import org.example.coinwatch.dto.AveragePricesDTO;
 import org.example.coinwatch.dto.CandleChartDTO;
+import org.example.coinwatch.dto.ChartType;
 import org.example.coinwatch.respository.CryptoPriceHistoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
 import java.util.List;
 
+@Service
 public class CryptoChartService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CryptoChartService.class);
 
     @Autowired
     CryptoPriceHistoryRepository cryptoPriceHistoryRepository;
 
-    public List<CandleChartDTO> getCandleChart(String cryptoId, String interval, ZonedDateTime from, ZonedDateTime to) {
+    public List<CandleChartDTO> getCandleChartData(String cryptoId, String interval, ZonedDateTime from, ZonedDateTime to) {
+        return cryptoPriceHistoryRepository.findCandleChart(interval, cryptoId, from, to)
+                .orElseThrow(() -> new RuntimeException("No candle data"));
+    }
 
-        return cryptoPriceHistoryRepository.findCandleChart(interval, cryptoId, from, to).orElseThrow(() -> new RuntimeException("Something wrong with candle chart"));
+    public List<AveragePricesDTO> getAveragePriceChartData(String cryptoId, String interval, ZonedDateTime from, ZonedDateTime to) {
+        return cryptoPriceHistoryRepository.findAveragePrices(interval, cryptoId, from, to)
+                .orElseThrow(() -> new RuntimeException("No average price data"));
+    }
+
+    public List<?> getChartData(String cryptoId, String interval, ZonedDateTime from, ZonedDateTime to, ChartType chartType) {
+        switch (chartType) {
+            case CANDLE:
+                return getCandleChartData(cryptoId, interval, from, to);
+            case LINE:
+            case BAR:
+                return getAveragePriceChartData(cryptoId, interval, from, to);
+            default:
+                throw new IllegalArgumentException("Unsupported chart type");
+        }
     }
 }
